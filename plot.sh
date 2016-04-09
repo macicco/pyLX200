5458ac8c62f285d5bd7686ad71839cfa3376bd95
nachoplus <mas.ignacio@gmail.com>
2016-04-09 20:11:01 +0200

    ramps plot script

---
+++ plot.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+gnuplot << EOF
+h=1200
+set term png size h*4/3,h
+set output "RA.png"
+set key autotitle columnhead
+set y2tics
+set autoscale y2
+set title "RA Axis"
+plot "RA.log" u 1:2 w steps,"RA.log" u 1:3 w steps,"RA.log" u 1:4 w lines,"RA.log" u 1:5 w lines,"RA.log" u 1:6 w lines lw 2,"RA.log" u 1:7 w lines lw 2,"RA.log" u 1:8 w lines lw 2,"RA.log" u 1:9  axes x2y2   
+set output "DEC.png"
+set title "DEC Axis"
+set key autotitle columnhead
+plot "DEC.log" u 1:2 w steps,"DEC.log" u 1:3 w steps,"DEC.log" u 1:4 w lines,"DEC.log" u 1:5 w lines,"DEC.log" u 1:6 w lines lw 2,"DEC.log" u 1:7 w lines lw 2,"DEC.log" u 1:8 w lines lw 2,"DEC.log" u 1:9  axes x2y2
+
+EOF
