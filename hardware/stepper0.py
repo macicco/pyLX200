357d180f402bae97ae623807ec1e3596c5dc930d
nachoplus <mas.ignacio@gmail.com>
2016-12-21 01:01:47 +0100

    stepper0 works syncroniusly

--- hardware/stepper0.py
+++ hardware/stepper0.py
@@ -5,7 +5,8 @@ import time,datetime
 import pigpio
 
 '''
-Stepper class
+Stepper classes.
+
 Units:
 	beta:steps
 	betaTarget:steps
@@ -20,7 +21,7 @@ interface:
 * 
 '''
 
-TIMESLOT=320000
+TIMESLOT=32000
 
 class engine2steppers(object):
 	def __init__(self,stepper0,stepper1):	
@@ -35,8 +36,10 @@ class engine2steppers(object):
 		self.s1.setRPM(rpm1)
 
 	def move(self,steps0,steps1):
+	   self.s0.move(steps0)
+	   self.s1.move(steps1)
 	   while not self.s0.syncronized or not self.s1.syncronized :
-		self.s0.waveChunk()
+		print self.s0.waveChunk()
 		self.s1.waveChunk()
 		self.sendWave()
 
@@ -48,7 +51,7 @@ class engine2steppers(object):
 		self.pi.wave_send_once(wid)
 
 		if self.old_wid !=-9999:
-			print "DEL:",self.pi.wave_delete(self.old_wid)
+			self.pi.wave_delete(self.old_wid)
 
 		self.old_wid=wid
 
@@ -56,12 +59,11 @@ class engine2steppers(object):
 		maxCBS=self.pi.wave_get_max_cbs()
 		maxPulse=self.pi.wave_get_max_pulses()
 		maxMicros=self.pi.wave_get_max_micros()
-		print "MAX:",maxCBS,maxPulse,maxMicros
-		
+	
 		CBS=self.pi.wave_get_cbs()
 		Pulse=self.pi.wave_get_pulses()
 		Micros=self.pi.wave_get_micros()
-		print "ACTUAL:",CBS,Pulse,Micros	
+		print "CBS Pulses, Micros (ACTUAL/MAX)",CBS,maxCBS,Pulse,maxPulse,Micros,maxMicros
 
 
 	def clearWave(self):	
@@ -73,7 +75,7 @@ class stepper(object):
 		self.pi=pigpio.pi('192.168.1.11')
 		self.setPINs(STEP_PIN,DIR_PIN,ENABLE_PIN,FAULT_PIN)
 		cb1 = self.pi.callback(self.STEP_PIN, pigpio.RISING_EDGE, self.stepCounter)
-		cb2 = self.pi.callback(self.FAULT_PIN, pigpio.RISING_EDGE, self.fault)
+		cb2 = self.pi.callback(self.FAULT_PIN, pigpio.EITHER_EDGE, self.fault)
 		self.stepsPerTurn=stepsPerTurn
 		self.microsteps=16
 		self.name=name
@@ -83,6 +85,7 @@ class stepper(object):
 		self.betaTarget=0
 		self.rpm=0
 		self.freq=0
+		self.steps_rest=0
 		self.syncronized=False
 
 	def setPINs(self,STEP_PIN,DIR_PIN,ENABLE_PIN,FAULT_PIN):
@@ -104,7 +107,10 @@ class stepper(object):
 		self.freq=rpm*self.microsteps*self.stepsPerTurn/60
 
 	def move(self,steps):
-		self.betaTarget=self.betaTarget+steps
+	   	self.syncronized=False
+		print self.betaTarget,self.beta,steps
+		self.betaTarget=self.beta+steps
+
 
 
 	def stepCounter(self,gpio, level, tick):
@@ -115,8 +121,9 @@ class stepper(object):
 			dir=-1
 		self.betaR=self.betaR+1*dir
 
-	def fault(self):
-		print "fault"
+	def fault(self,gpio, level, tick):
+		if level==1:
+			print "fault"
 
 
 	def auxPINs(self,RESET_PIN,SLEEP_PIN,M0_PIN,M1_PIN,M2_PIN):
@@ -137,17 +144,35 @@ class stepper(object):
 
 
 	def waveChunk(self):
-		dir=self.dir
-		lasting=TIMESLOT
-		if self.freq!=0:
-	 		t=500000/self.freq   #time in micros
-			steps=int(self.freq*lasting/1000000.)
-		else:
-			steps=0
-			return self.beta
-
-		print lasting,steps,self.freq
 		wave=[]
+		lasting=TIMESLOT
+		delta=self.betaTarget-self.beta
+		if delta>=0:
+			self.dir=1
+		else:
+			self.dir=-1
+		dir=self.dir
+		if delta==0 or self.freq==0:
+			steps=0
+			if delta==0:
+				self.syncronized=True
+				print self.name,"Syncronize"
+			wave.append(pigpio.pulse(0, 1<<self.STEP_PIN, lasting))
+			self.pi.wave_add_generic(wave)
+			return 	self.beta
+	
+ 		t=500000/self.freq   #time in micros
+		steps_float=self.freq*lasting/1000000.
+		steps=int(steps_float)
+		self.steps_rest=self.steps_rest+(steps_float-steps)
+		if self.steps_rest>=1:
+			#print "STEP REST",int(self.steps_rest),self.steps_rest
+			steps=steps+int(self.steps_rest)
+			self.steps_rest=self.steps_rest-int(self.steps_rest)
+
+		if abs(delta)<steps:
+			steps=abs(delta)
+
 		for i in range(steps):
 			wave.append(pigpio.pulse(0, 1<<self.STEP_PIN, t))
 			wave.append(pigpio.pulse(1<<self.STEP_PIN, 0, t))
@@ -156,14 +181,12 @@ class stepper(object):
 		if dir>0:
 			dirwave.append(pigpio.pulse(0,1<<self.DIR_PIN,t/2))
 			dirwave.append(pigpio.pulse(1<<self.DIR_PIN,0,lasting-t/2))
-			print "Forward"
 		else:
 			dirwave.append(pigpio.pulse(0,1<<self.DIR_PIN,t/2))
 			dirwave.append(pigpio.pulse(0,1<<self.DIR_PIN,lasting-t/2))
-			print "Backward"
 		pulses=self.pi.wave_add_generic(dirwave)
 		#self.checkWaveBuffer()
-		print "Wave chunck lasting/cbs:",t*2.*steps/1000000,pulses
+		#print "Wave chunck lasting/cbs:",t*2.*steps/1000000,pulses
 		self.beta=self.beta+steps*dir
 		return self.beta
 		
@@ -177,8 +200,10 @@ if __name__ == '__main__':
 	dec=stepper("DEC",48,18,22,8,17)
 	dec.auxPINs(25,24,11,9,10,)
 	e=engine2steppers(ra,dec)
-	e.rpm(1,60)
-	e.move(1,1)		
+	e.rpm(60*4,60*4)
+	e.move(200*16*5,48*16*5)
+	print "======================="
+	e.move(-200*16*5,-48*16*5)				
 '''
 '''
 
