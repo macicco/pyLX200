1162358ee1eef186b1feab4cb9b54ff1add9faa5
nachoplus <mas.ignacio@gmail.com>
2016-12-23 17:45:12 +0100

    DRV8825 works! Added rotate function

--- hardware/stepper0.py
+++ hardware/stepper0.py
@@ -5,31 +5,36 @@ import time,datetime
 import pigpio
 
 '''
-Stepper classes.
+Stepper classes. It use the 'wave' funtionality include in 
+pigpio library to send pulses to a drv8825 driver.
 
-Units:
-	beta:steps
-	betaTarget:steps
-	v:steps/s
-	a:steps/s**2
+pigpio only has one DMA channel dedicate to wave. Thus we need
+to compose all waves for all motos prior to send them throught
+DMA channel. It is done in chuncks of pulses. As side effect is 
+not posible to driver a single motor without command the others. 
+For the same reason motor are perfectly syncronice each other.
+
+
 
 interface:
 
-* move(steps,dir) -> move n steps relative to the current position
-* goto(radians)   -> go to an absolute position in radians
-* speed(rpm)      -> set speed
+* move(steps0,steps1) 		-> move n steps relative to the current position
+* rotate(degrees0,degrees0) 	-> go to an absolute position in degrees
+* rmp(rpm0,rpm1)        	-> set speed
 * 
 '''
 
-TIMESLOT=32000
+TIMESLOT=10000
+raspberry='192.168.1.11'
 
 class engine2steppers(object):
 	def __init__(self,stepper0,stepper1):	
-		self.pi=pigpio.pi('192.168.1.11')
+		self.pi=pigpio.pi(raspberry)
 		self.pi.wave_clear()
 		self.s0=stepper0
 		self.s1=stepper1
 		self.old_wid=-9999
+
 
 	def rpm(self,rpm0,rpm1):
 		self.s0.setRPM(rpm0)
@@ -38,8 +43,16 @@ class engine2steppers(object):
 	def move(self,steps0,steps1):
 	   self.s0.move(steps0)
 	   self.s1.move(steps1)
+	   self._move()
+
+	def rotate(self,degrees0,degrees1):
+	   self.s0.rotate(degrees0)
+	   self.s1.rotate(degrees1)
+	   self._move()
+
+	def _move(self):
 	   while not self.s0.syncronized or not self.s1.syncronized :
-		print self.s0.waveChunk()
+		self.s0.waveChunk()
 		self.s1.waveChunk()
 		self.sendWave()
 
@@ -68,16 +81,17 @@ class engine2steppers(object):
 
 	def clearWave(self):	
 		self.pi.wave_clear()
-	
+
+class engine2steppers(object):	
 
 class stepper(object):
 	def __init__(self,name,stepsPerTurn,STEP_PIN,DIR_PIN,ENABLE_PIN,FAULT_PIN):
-		self.pi=pigpio.pi('192.168.1.11')
+		self.pi=pigpio.pi(raspberry)
 		self.setPINs(STEP_PIN,DIR_PIN,ENABLE_PIN,FAULT_PIN)
 		cb1 = self.pi.callback(self.STEP_PIN, pigpio.RISING_EDGE, self.stepCounter)
 		cb2 = self.pi.callback(self.FAULT_PIN, pigpio.EITHER_EDGE, self.fault)
 		self.stepsPerTurn=stepsPerTurn
-		self.microsteps=16
+		self.microsteps=32
 		self.name=name
 		self.dir=1
 		self.beta=0
@@ -108,10 +122,13 @@ class stepper(object):
 
 	def move(self,steps):
 	   	self.syncronized=False
-		print self.betaTarget,self.beta,steps
 		self.betaTarget=self.beta+steps
 
-
+	def rotate(self,degrees):
+	   	self.syncronized=False
+		target=degrees*(self.stepsPerTurn/360.)*self.microsteps
+		#print target,self.betaTarget,self.beta
+		self.betaTarget=target
 
 	def stepCounter(self,gpio, level, tick):
 		dir=self.pi.read(self.DIR_PIN)
@@ -120,6 +137,7 @@ class stepper(object):
 		else:
 			dir=-1
 		self.betaR=self.betaR+1*dir
+		#print self.betaR
 
 	def fault(self,gpio, level, tick):
 		if level==1:
@@ -132,15 +150,13 @@ class stepper(object):
 		pi.set_mode(M1_PIN, pigpio.OUTPUT)
 		pi.set_mode(M0_PIN, pigpio.OUTPUT)
 		pi.write(M2_PIN, True)
-		pi.write(M1_PIN, False)
-		pi.write(M0_PIN,False)
+		pi.write(M1_PIN, True)
+		pi.write(M0_PIN,True)
 
 		pi.set_mode(RESET_PIN, pigpio.OUTPUT)
 		pi.set_mode(SLEEP_PIN, pigpio.OUTPUT)
 		pi.write(RESET_PIN, True)
 		pi.write(SLEEP_PIN, True)
-
-
 
 
 	def waveChunk(self):
@@ -156,22 +172,33 @@ class stepper(object):
 			steps=0
 			if delta==0:
 				self.syncronized=True
-				print self.name,"Syncronize"
+				#print self.name,"Syncronize"
 			wave.append(pigpio.pulse(0, 1<<self.STEP_PIN, lasting))
 			self.pi.wave_add_generic(wave)
 			return 	self.beta
 	
- 		t=500000/self.freq   #time in micros
-		steps_float=self.freq*lasting/1000000.
+
+		steps_float=self.freq*lasting/1000000
 		steps=int(steps_float)
 		self.steps_rest=self.steps_rest+(steps_float-steps)
 		if self.steps_rest>=1:
-			#print "STEP REST",int(self.steps_rest),self.steps_rest
+			print "STEP REST",int(self.steps_rest),self.steps_rest
 			steps=steps+int(self.steps_rest)
 			self.steps_rest=self.steps_rest-int(self.steps_rest)
 
 		if abs(delta)<steps:
-			steps=abs(delta)
+			print "Delta",steps,delta,self.betaTarget,self.beta
+			steps=int(abs(delta))
+
+
+		if steps==0:
+			wave.append(pigpio.pulse(0, 1<<self.STEP_PIN, lasting))
+			self.pi.wave_add_generic(wave)
+			return 	self.beta
+
+ 		#t0=500000/self.freq   #time in micros
+		t=0.5*lasting/steps   #time in micros
+		#print self.name,t0,t,steps,self.freq,dir
 
 		for i in range(steps):
 			wave.append(pigpio.pulse(0, 1<<self.STEP_PIN, t))
@@ -201,9 +228,25 @@ if __name__ == '__main__':
 	dec.auxPINs(25,24,11,9,10,)
 	e=engine2steppers(ra,dec)
 	e.rpm(60*4,60*4)
-	e.move(200*16*5,48*16*5)
+	e.rotate(180,0)
+	#time.sleep(1)
+	e.rotate(0,0)
+	#time.sleep(1)
+	d=1
+	while True:
+		q0=int(200*32*180/(360))
+		q1=int(48*32*180/(360))
+		e.move(d*q0,d*q1)
+		#time.sleep(0.1)
+		d=d*-1
+	e.rpm(300,0)		
+	e.move(200*16*50,48*16*5)
 	print "======================="
+	e.rpm(60*1,60*1)
 	e.move(-200*16*5,-48*16*5)				
+	print "======================="
+	e.rpm(0.1,1)
+	e.move(200*16,48*16)				
 '''
 '''
 
